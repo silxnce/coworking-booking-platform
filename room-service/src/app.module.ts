12230432c5dd32496bf69f7877fc3b461dc47786
Ivan Klochay <132347144+silxnce@users.noreply.github.com>
2025-05-06 03:11:50 +0300

    Creating architecture: room-service

---
+++ room-service/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { RoomModule } from './api/room/room.module';
+import { DatabaseModule } from './database/database.module';
+
+@Module({
+  imports: [RoomModule, DatabaseModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
