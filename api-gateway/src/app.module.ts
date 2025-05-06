1e2271c6c90b3ce601b7325ebffb437356ac1a2b
Ivan Klochay <132347144+silxnce@users.noreply.github.com>
2025-05-06 03:10:18 +0300

    Creating architecture: API Gateway

---
+++ api-gateway/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './api/user/user.module';
+import { RoomModule } from './api/room/room.module';
+import { BookingModule } from './api/booking/booking.module';
+
+@Module({
+  imports: [UserModule, RoomModule, BookingModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
