ff5f08ce40ee148786a5985245955a30d2c71574
Ivan Klochay <132347144+silxnce@users.noreply.github.com>
2025-05-06 03:12:59 +0300

    Creating architecture: booking-service

---
+++ booking-service/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BookingModule } from './api/booking/booking.module';
+import { DatabaseModule } from './database/database.module';
+
+@Module({
+  imports: [BookingModule, DatabaseModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
